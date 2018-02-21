d9ed17fcf5636c015f546f48dce579ac503b091a
Newton Bii <biinewton382@gmail.com>
2018-02-21 18:31:26 +0300

    update settings and views modules.

--- farm_to_pocket/config.py
+++
@@ -1,2 +0,0 @@
-username = 'Newton Bii'
-apikey = 'ead97b13ac85817f51d12d7f442c6b552fd7b6d22da7b719d9670687accc8d11'
--- farm_to_pocket/views.py
+++ farm_to_pocket/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 from africastalking.AfricasTalkingGateway import (AfricasTalkingGateway, AfricasTalkingGatewayException)
+from .config import username, apikey
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
+def callback(request):
+    if request.method == 'POST' and request.POST:
+        sessionID = request.POST.get('sessionID')
+        serviceCode = request.POST.get('serviceCode')
+        phoneNumber = request.POST.get('phoneNumber')
+        text = request.POST.get('text')
+        now = datetime.datetime.now()
