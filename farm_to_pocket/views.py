09b7f338f920d497d861c2c2122feb1af31acc52
Newton Bii <biinewton382@gmail.com>
2018-02-22 15:33:39 +0300

    Update views module.

--- farm_to_pocket/views.py
+++ farm_to_pocket/views.py
@@ -1,34 +1,32 @@
 from django.shortcuts import render
-from .AfricasTalkingGateway import AfricasTalkingGateway, AfricasTalkingGatewayException
+from africastalking.AfricasTalkingGateway import AfricasTalkingGateway, AfricasTalkingGatewayException
 from .config import username, apikey
 from django.views.decorators.csrf import csrf_exempt
 from .models import User, session_levels
+import datetime
 # Create your views here.
-# @csrf_exempt
+@csrf_exempt
 def callback(request):
+
     if request.method == 'POST' and request.POST:
-        sessionID = request.POST.get('sessionID')
-        serviceCode = request.POST.get('serviceCode')
-        phoneNumber = request.POST.get('phoneNumber')
-        text = request.POST.get('text')
+        sessionId = request.POST.get('sessionId')
+        serviceCode=request.POST.get('serviceCode')
+        phoneNumber=request.POST.get('phoneNumber')
+        text=request.POST.get('text')
         now = datetime.datetime.now()
 
         textList = text.split('*')
         userResponse = textList[-1].strip()
 
-        #Check level of user and if user exists
-        try:
-            user = User.objects.get(phonenumber=phoneNumber)
-        except User.DoesNotExist as e:
-            level = 0
+        if level == 0:
+            if userResponse == "":
+                session_level1 = User.objects.get(phonenumber=phoneNumber)
+                session_level1.level=1
+                session_level1.save()
 
-        try:
-            result, created = User.objects.get_or_create(phonenumber=phoneNumber)
-        if created:
-            result.save()
-        if userResponse == '':
-            response = 'CON Welcome to FarmToPocket Service. Would you like to buy or sell?'
-            response += '1. Buyer'
-            response += '2. Seller'
-            if userResponse == '1':
-                response = 'Please enter your full name'
+                response = "CON Welcome to FarmerService. Are you buying or selling?"
+                response += "1. Buyer. \n"
+                response += "2. Seller. \n"
+
+                return HttpResponse(response, content_type='text/plain')
+                
