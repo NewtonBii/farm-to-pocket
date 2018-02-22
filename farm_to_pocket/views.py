2a6edf5ad21585da7cb7d08f442d6fadda180eda
Newton Bii <biinewton382@gmail.com>
2018-02-22 15:07:18 +0300

    Create requirements.txt file.

--- farm_to_pocket/views.py
+++ farm_to_pocket/views.py
@@ -24,42 +24,11 @@ def callback(request):
 
         try:
             result, created = User.objects.get_or_create(phonenumber=phoneNumber)
-            if created:
-                result.save()
+        if created:
+            result.save()
         if userResponse == '':
             response = 'CON Welcome to FarmToPocket Service. Would you like to buy or sell?'
             response += '1. Buyer'
             response += '2. Seller'
             if userResponse == '1':
                 response = 'Please enter your full name'
-                if level == 1:
-
-        # try:
-        #     session = User.objects.get(phonenumber=phoneNumber)
-        #     level = session.level
-        # except User.DoesNotExist as e:
-        #     level = 0
-        #
-        # # Serve basic menu
-        # if level == 0 or level == 1:
-        #     if userResponse == '':
-        #         if level == 0:
-        #             session_level1 = User.objects.get(phonenumber=phoneNumber)
-        #             session_level1.level = 1
-        #             session_level1.save()
-        #
-        #             response = 'CON Welcome to FarmToPocket. would you like to buy or sell?'
-        #             response += '1. Buyer.\n'
-        #             response += '2. Seller.\n'
-        #
-        #             return HttpResponse(response, content_type='text/plain')
-        #     if userResponse == '1':
-        #         if level == 1:
-        #             response = 'CON What would you like to sell?'
-        #             response +=  '1. Beans.\n'
-        #             response +=  '2. Maize.\n'
-        #             response +=  '3. Wheat.\n'
-        #             response +=  '4. Milk.\n'
-        #             response +=  '5. Sukuma Wiki.\n'
-        #             response +=  '6. Cabbage.\n'
-        #             response +=  '7. Managu.\n'
