1089bcf53e1ac2094ef08370e77d56c92f359033
Newton Bii <biinewton382@gmail.com>
2018-02-26 08:03:56 +0300

    Update the callback function.

--- farm_to_pocket/views.py
+++ farm_to_pocket/views.py
@@ -36,8 +36,19 @@ def callback(request):
             response += "2. Seller\n"
             return HttpResponse(response, content_type='text/plain')
         if userResponse == "1":
-            response = "CON Enter your name:\n"
-            return HttpResponse(response, content_type='text/plain')
+            created,result = User.objects.get_or_create(phonenumber=phoneNumber)
+            if created:
+                result.save()
+                response = "CON Enter your name:\n"
+                return render(response, content_type='text/plain')
+            if not created:
+                if not result.name:
+                    result.name = userResponse
+                    result.save()
+
+                    response = "CON Where do you live?\n"
+                    return render(response, content_type='text/plain')
+                    
         if userResponse == "2":
             response = "CON Enter your name:\n"
     return render(request, 'index.html')
