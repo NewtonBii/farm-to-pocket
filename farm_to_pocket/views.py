88df09cc3cc1bbe55a4d6992de2e3e1c7a82dafe
Newton Bii <biinewton382@gmail.com>
2018-02-26 07:50:57 +0300

    Update the callback function.

--- farm_to_pocket/views.py
+++ farm_to_pocket/views.py
@@ -22,22 +22,22 @@ def callback(request):
         userResponse = textList[-1].strip()
 
 
-        try:
-            session = session_levels.objects.get(session_id=sessionId)
-            user = User.objects.get(phonenumber=phoneNumber)
-        except user.DoesNotExist and session.DoesNotExist:
-            if user:
-                if session:
-                    level = session.level
-                    if level == 0:
-                        if userResponse == "":
-                            response = "CON Welcome to our Service. Are you a buyer or a seller?\n"
-                            response += "1. Buyer\n"
-                            response += "2. Seller\n"
-                            return HttpResponse(response, content_type='text/plain')
-                        if userResponse == "1":
-                            response = "CON Enter your name:\n"
-                            return HttpResponse(response, content_type='text/plain')
-                        if userResponse == "2":
-                            response = "CON Enter your name:\n"
+        # try:
+        #     session = session_levels.objects.get(session_id=sessionId)
+        #     user = User.objects.get(phonenumber=phoneNumber)
+        # except user.DoesNotExist and session.DoesNotExist:
+        #     if user:
+        #         if session:
+        #             level = session.level
+        #             if level == 0:
+        if userResponse == "":
+            response = "CON Welcome to our Service. Are you a buyer or a seller?\n"
+            response += "1. Buyer\n"
+            response += "2. Seller\n"
+            return HttpResponse(response, content_type='text/plain')
+        if userResponse == "1":
+            response = "CON Enter your name:\n"
+            return HttpResponse(response, content_type='text/plain')
+        if userResponse == "2":
+            response = "CON Enter your name:\n"
     return render(request, 'index.html')
