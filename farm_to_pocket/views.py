8e4ab19fc11869736e1a8a45d7a8ce3b319e7c6b
Newton Bii <biinewton382@gmail.com>
2018-03-01 07:11:52 +0300

    Restructure the menu.

--- farm_to_pocket/views.py
+++ farm_to_pocket/views.py
@@ -30,9 +30,9 @@ def callback(request):
 
         if level == 0:
             if userResponse == "":
-                response = "CON Welcome to our Service. Are you a Buyer or a seller?\n"
-                response += "1. Buyer\n"
-                response += "2. Seller\n"
+                response = "CON Welcome to onPoint Service. Would you like to buy or sell?\n"
+                response += "1. I am Buying\n"
+                response += "2. I am a Seller\n"
 
                 return HttpResponse(response, content_type='text/plain')
 
@@ -40,7 +40,7 @@ def callback(request):
             session_level1.level=1
             session_level1.type_of_user = userResponse
             session_level1.save()
-            response = "CON Enter your name:\n"
+            response = "CON Please enter your name:\n"
 
             return HttpResponse(response, content_type='text/plain')
 
@@ -49,14 +49,14 @@ def callback(request):
             session_level2.level=2
             session_level2.name = userResponse
             session_level2.save()
-            response = "CON Enter your County e.g. Nairobi, Uasin Gishu.\n"
+            response = "CON Which is your county? \n e.g. Nairobi, Uasin Gishu, Machakos\n"
             return HttpResponse(response, content_type='text/plain')
         if level == 2:
             session_level3 = User.objects.get(phonenumber = phoneNumber)
             session_level3.level=3
             session_level3.location = userResponse
             session_level3.save()
-            response = "CON Enter your closest town center.\n"
+            response = "CON What is your closest town or market center?.\n e.g. Kijabe Town or Ngong Town"
             return HttpResponse(response, content_type='text/plain')
 
         if level == 3:
@@ -64,35 +64,31 @@ def callback(request):
             session_level4.level=4
             session_level4.nearest_town = userResponse
             session_level4.save()
-            response = "CON What would you like to buy?\n"
+            response = "CON Enter the product name\ne.g. Maize, Beans, Milk"
             return HttpResponse(response, content_type='text/plain')
 
         if level == 4:
             session_level5 = User.objects.get(phonenumber = phoneNumber)
-            # product = Product.objects.create(user=user)
             session_level5.level=5
-            response = "CON What quantity would you like?\n"
+            response = "CON Enter the quantity: in Kgs\n e.g 1, 10, 100"
             return HttpResponse(response, content_type='text/plain')
 
         if level == 5:
             session_level6 = User.objects.get(phonenumber = phoneNumber)
-            # product = Product.objects.create(user=user)
             session_level6.level=6
-            response = "CON How much are you willing to pay?\n. e.g. 100 per Kg "
+            response = "CON Enter the price per Kg\n. e.g. 50, 100, 1000"
             return HttpResponse(response, content_type='text/plain')
 
         if level == 6:
             session_level7 = User.objects.get(phonenumber = phoneNumber)
-            # product = Product.objects.create(user=user)
             session_level7.level=7
-            response = "CON When would you like to have your product? e.g. 1 day, 1 week, 1 month...\n"
+            response = "CON When is it available?\n When do you need it?\n e.g. 1 day, 1 week, 1 month...\n"
             return HttpResponse(response, content_type='text/plain')
 
         if level == 7:
             session_level8 = User.objects.get(phonenumber = phoneNumber)
-            # product = Product.objects.create(user=user)
             session_level8.level=8
-            response = "END Thank you for using the service. Your response has been recorded.\n"
+            response = "END Your request has been recieved. \n We will send you a message with contact details of a buyer/seller that matches your request."
             product = Product.objects.create(name = textList[4], quantity = textList[5], price=textList[6], user=user, availability = textList[7])
             product.save()
             print(textList[4])
